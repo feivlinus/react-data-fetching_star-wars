bc75317da55efeb000dc5ca919d20bb6a307d840
Felix Schmidt <para.feivl.dox@gmail.com>
2023-10-30 11:37:09 +0100

    Challenge completed

---
+++ pages/characters/[id].js
@@ -0,0 +1,34 @@
+import Card from "../../components/Card";
+import Layout from "../../components/Layout";
+import useSWR from "swr";
+import { useRouter } from "next/router";
+
+export default function Character() {
+  const router = useRouter();
+  const { id } = router.query;
+  const URL = `https://swapi.dev/api/people/${id}`;
+
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+  const { data, error, isLoading } = useSWR(URL, fetcher);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (error) {
+    return <h1>There is an Error. Try again later.</h1>;
+  }
+
+  return (
+    <Layout>
+      <Card
+        id={id}
+        name={data.name}
+        height={data.height}
+        eyeColor={data.eye_color}
+        birthYear={data.birth_year}
+      />
+    </Layout>
+  );
+}
